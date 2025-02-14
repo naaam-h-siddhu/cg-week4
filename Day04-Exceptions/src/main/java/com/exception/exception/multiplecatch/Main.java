ec0eedf697b1c5117ce734ef4b076d593ba5eea5
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-14 13:54:12 +0530

    ✨ [Sudhakar] Add . Added the Unchecked exception handling program

---
+++ Day04-Exceptions/src/main/java/com/exception/exception/multiplecatch/Main.java
@@ -0,0 +1,16 @@
+package com.exception.exception.multiplecatch;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = {1,2,3};
+        int lastnumber = -1;
+        try{
+            lastnumber = array[3];
+            System.out.println("value at 3: "+lastnumber);
+        }catch (NullPointerException e){
+            System.out.println("Array is not initialized ");
+        }catch (IndexOutOfBoundsException e){
+            System.out.println("Invalid Index !");
+        }
+    }
+}
