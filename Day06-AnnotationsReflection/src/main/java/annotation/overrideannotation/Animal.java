436c9fbfa7632efde2019e2c2b95ed834fa9e0bd
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-17 15:28:04 +0530

    ✨[Sudhakar] Add . Added the program to perform override annotation

---
+++ Day06-AnnotationsReflection/src/main/java/annotation/overrideannotation/Animal.java
@@ -0,0 +1,7 @@
+package annotation.overrideannotation;
+
+public class Animal {
+    public void makeSound(){
+        System.out.println("Woof Woof !!1");
+    }
+}
---
+++ Day06-AnnotationsReflection/src/main/java/annotation/overrideannotation/Dog.java
@@ -0,0 +1,8 @@
+package annotation.overrideannotation;
+
+public class Dog extends Animal{
+    @Override
+    public void makeSound(){
+        System.out.println("Woof Woof Woof Woof!!");
+    }
+}
