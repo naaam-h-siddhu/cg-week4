696a363b004d85613e8f92eb579de408bf587b61
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-17 21:48:09 +0530

    ✨[Sudhakar] Add . Added the program to generate object json

---
+++ Day06-AnnotationsReflection/src/main/java/reflection/advance/jsongenerator/Address.java
@@ -0,0 +1,15 @@
+package reflection.advance.jsongenerator;
+
+public class Address {
+    private String city;
+    private int pin ;
+
+    public Address(){
+
+    }
+
+    public Address(String city, int pin) {
+        this.city = city;
+        this.pin = pin;
+    }
+}
