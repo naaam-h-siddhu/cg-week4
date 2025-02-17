adde31a0b96c4511e02a22fcc9b03e9d6e429840
naaam-h-siddhu <sudhakarsingh2004@gmail.com>
2025-02-18 00:08:58 +0530

    ✨[Sudhakar] Add . Added the program to create a custom annotation of verify the username

---
+++ Day06-AnnotationsReflection/src/main/java/annotation/practice/intermediate/maxlength/MaxLength.java
@@ -0,0 +1,9 @@
+package annotation.practice.intermediate.maxlength;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MaxLength {
+    int value();
+}
